8907ce40338aa1a5cfe508dd937ef2e7cd8dc32e
ardistyaa <ardistyaaa@gmail.com>
2023-10-20 19:47:58 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+if(window.innerWidth >= 575){
+    document.body.innerHTML = "Keneke gur nek HP WKWKWKWK" 
+}
+window.addEventListener('resize', ()=>{
+    if(window.innerWidth >= 575){
+        document.body.innerHTML = "Keneke gur nek HP WKWKWKWK" 
+    }
+})
